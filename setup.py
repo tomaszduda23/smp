69a997ae1e3838b477fbb6901fa534499515d859
Tomasz Duda <tomaszduda23@gmail.com>
2024-05-06 00:51:27 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='smp',
+      version='0.1.9',
+      description='smp',
+      author='JP Hutchins',
+      url='https://github.com/JPHutchins/smp',
+      packages=[],
+     )
